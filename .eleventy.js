16f9c60474c8e9a93322cd9e9e15b9e667132ebf
Ria <valerieria@outlook.com>
2023-06-21 11:44:18 +0800

    Add nojekyll and output command

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 
 module.exports = function(config) {
+  dir: {
+    input: "src",
+    output: "docs"
+}
 
   // A useful way to reference the context we are runing eleventy in
   let env = process.env.ELEVENTY_ENV;
